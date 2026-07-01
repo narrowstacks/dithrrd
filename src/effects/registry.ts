42874226ab244db98ca099836d7af9d84cc61c90
aaron f.a <aaron@affords.art>
2026-07-01 02:16:05 -0700

    feat: add circular halftone effect

--- src/effects/registry.ts
+++ src/effects/registry.ts
@@ -2,9 +2,10 @@ import type { Effect } from '@/effects/types'
 import { grade } from '@/effects/grade'
 import { pixelate } from '@/effects/pixelate'
 import { bayer } from '@/effects/bayer'
+import { halftone } from '@/effects/halftone'
 import { floyd } from '@/effects/floydSteinberg'
 
-export const EFFECT_LIST: Effect[] = [grade, pixelate, bayer, floyd]
+export const EFFECT_LIST: Effect[] = [grade, pixelate, bayer, halftone, floyd]
 
 export const registry: Record<string, Effect> = Object.fromEntries(
   EFFECT_LIST.map((e) => [e.type, e]),
